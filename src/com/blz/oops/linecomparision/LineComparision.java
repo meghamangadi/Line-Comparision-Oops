340fb913596743e2a2549ddb13c8b0cfc1da3ad5
megha angadi <meghamangadi@gmail.com>
2021-09-02 17:33:24 +0530

    Added: Methods to find Equality of two lines

--- src/com/blz/oops/linecomparision/LineComparision.java
+++ src/com/blz/oops/linecomparision/LineComparision.java
@@ -3,14 +3,15 @@ package com.blz.oops.linecomparision;
 import java.util.Scanner;
 
 public class LineComparision {
-	public static int x1, x2, y1, y2;
-	public static double distance;
+	public static int x1, x2, x3, x4, y1, y2, y3, y4;
+	public static double distance1, distance2;
 
 	public static void main(String[] args) {
 
 		welcome();
 		userInput();
 		distance();
+		equal();
 	}
 
 	static void welcome() {
@@ -19,6 +20,8 @@ public class LineComparision {
 
 	public static void userInput() {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("For line 1");
+
 		System.out.println("Enter the value of X1");
 		x1 = sc.nextInt();
 
@@ -30,11 +33,36 @@ public class LineComparision {
 
 		System.out.println("Enter the value of Y2");
 		y2 = sc.nextInt();
+
+		System.out.println("For line 2");
+
+		System.out.println("Enter the value of X1");
+		x3 = sc.nextInt();
+
+		System.out.println("Enter the value of X2");
+		x4 = sc.nextInt();
+
+		System.out.println("Enter the value of Y1");
+		y3 = sc.nextInt();
+
+		System.out.println("Enter the value of Y2");
+		y4 = sc.nextInt();
+		sc.close();
 	}
 
 	public static void distance() {
-		distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-		System.out.println("Length of a line : " + +distance);
+		distance1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		distance2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+
+		System.out.println("Distance 1 :" + distance1);
+		System.out.println("Distance 2 :" + distance2);
 	}
 
+	public static void equal() {
+		if (distance1 == distance2) {
+			System.out.println("Length of two lines are equal");
+		} else {
+			System.out.println("Length of two lines are not equal");
+		}
+	}
 }
