9375580187c964bfcb02ced3f3733aaaa49b674b
megha angadi <meghamangadi@gmail.com>
2021-09-02 17:11:53 +0530

    Added :Welcome message to the user

---
+++ src/com/blz/oops/linecomparision/LineComparision.java
@@ -0,0 +1,12 @@
+package com.blz.oops.linecomparision;
+
+public class LineComparision {
+	public static void main(String[] args) {
+
+		welcome();
+	}
+
+	static void welcome() {
+		System.out.println("Welcome to Line Comparsion Computation Program");
+	}
+}
